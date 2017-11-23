f825d66e9af7c7ab939e5f460d7ce9dc2e4ab7d2
Xiaoyong Pan <xypan172436@gmail.com>
2017-11-23 16:09:59 +0100

    Create metric.py

---
+++ metric.py
@@ -0,0 +1,40 @@
+def hamming_score(y_true, y_pred, normalize=True, sample_weight=None):
+    '''
+    Compute the Hamming score (a.k.a. label-based accuracy) for the multi-label case
+    https://stackoverflow.com/q/32239577/395857
+    '''
+    acc_list = []
+    for i in range(y_true.shape[0]):
+        set_true = set( np.where(y_true[i])[0] )
+        set_pred = set( np.where(y_pred[i])[0] )
+        #print('\nset_true: {0}'.format(set_true))
+        #print('set_pred: {0}'.format(set_pred))
+        tmp_a = None
+        if len(set_true) == 0 and len(set_pred) == 0:
+            tmp_a = 1
+        else:
+            tmp_a = len(set_true.intersection(set_pred))/\
+                    float( len(set_true.union(set_pred)) )
+        #print('tmp_a: {0}'.format(tmp_a))
+        acc_list.append(tmp_a)
+    return np.mean(acc_list)
+
+if __name__ == "__main__":
+    print('Hamming score: {0}'.format(hamming_score(y_true, y_pred))) # 0.375 (= (0.5+1+0+0)/4)
+
+    # For comparison sake:
+    import sklearn.metrics
+
+    # Subset accuracy
+    # 0.25 (= 0+1+0+0 / 4) --> 1 if the prediction for one sample fully matches the gold. 0 otherwise.
+    print('Subset accuracy: {0}'.format(sklearn.metrics.accuracy_score(y_true, y_pred, normalize=True, sample_weight=None)))
+
+    # Hamming loss (smaller is better)
+    # $$ \text{HammingLoss}(x_i, y_i) = \frac{1}{|D|} \sum_{i=1}^{|D|} \frac{xor(x_i, y_i)}{|L|}, $$
+    # where
+    #  - \\(|D|\\) is the number of samples  
+    #  - \\(|L|\\) is the number of labels  
+    #  - \\(y_i\\) is the ground truth  
+    #  - \\(x_i\\)  is the prediction.  
+    # 0.416666666667 (= (1+0+3+1) / (3*4) )
+    print('Hamming loss: {0}'.format(sklearn.metrics.hamming_loss(y_true, y_pred))) 
