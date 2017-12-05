f6f695b875e4e5aa920b6e9d302c19595e5ad5b8
Xiaoyong Pan <xypan172436@gmail.com>
2017-12-05 22:00:25 +0100

    Update multilabel.py

--- multilabel.py
+++ multilabel.py
@@ -1,3 +1,26 @@
+y_classes = df_y.idxmax(1, skipna=False)
+
+from sklearn.preprocessing import LabelEncoder
+
+# Instantiate the label encoder
+le = LabelEncoder()
+
+# Fit the label encoder to our label series
+le.fit(list(y_classes))
+
+# Create integer based labels Series
+y_integers = le.transform(list(y_classes))
+
+# Create dict of labels : integer representation
+labels_and_integers = dict(zip(y_classes, y_integers))
+
+from sklearn.utils.class_weight import compute_class_weight, compute_sample_weight
+
+class_weights = compute_class_weight('balanced', np.unique(y_integers), y_integers)
+sample_weights = compute_sample_weight('balanced', y_integers)
+
+class_weights_dict = dict(zip(le.transform(list(le.classes_)), class_weights))
+
 
 clf = Sequential()
 
